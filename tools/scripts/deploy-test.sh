44e5728d9fae1db60d3b0fe8492619f38e62cb1d
myflowpl <pedro.blaszczak@gmail.com>
2019-03-10 15:53:51 +0100

    feat(web-server): added angular universal

--- tools/scripts/deploy-test.sh
+++ tools/scripts/deploy-test.sh
@@ -2,6 +2,6 @@
 
 echo 'TEST Deploy - Started'
 
-node ./node_modules/.bin/standard-version --prerelease BUILD.$TRAVIS_BUILD_NUMBER --skip.tag=true --skip.commit=true
+node ./node_modules/.bin/standard-version --prerelease $TRAVIS_BUILD_NUMBER --skip.tag=true --skip.commit=true
 node ./tools/scripts/copy-assets.js
 node ./node_modules/.bin/shipit test deploy
