4b0ad1a3acbade7aa058957ffd809c69978425f8
myflowpl <pedro.blaszczak@gmail.com>
2019-03-12 16:01:39 +0100

    ci: add no-verify on version bump

--- tools/scripts/deploy-prod.sh
+++ tools/scripts/deploy-prod.sh
@@ -2,7 +2,7 @@
 
 echo 'PROD Deploy - Started'
 
-node ./node_modules/.bin/standard-version
+node ./node_modules/.bin/standard-version --no-verify
 node ./tools/scripts/copy-assets.js
 node ./node_modules/.bin/shipit prod deploy
 git push --no-verify --follow-tags origin HEAD:master
