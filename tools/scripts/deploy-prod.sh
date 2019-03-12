9aee8985d9dcf963f3499b3dc6ac6e96c2463ccc
myflowpl <pedro.blaszczak@gmail.com>
2019-03-12 15:27:28 +0100

    ci: fix curcular builds after travis push

--- tools/scripts/deploy-prod.sh
+++ tools/scripts/deploy-prod.sh
@@ -5,4 +5,4 @@ echo 'PROD Deploy - Started'
 node ./node_modules/.bin/standard-version
 node ./tools/scripts/copy-assets.js
 node ./node_modules/.bin/shipit prod deploy
-git push --follow-tags origin HEAD:master
+git push --no-verify --follow-tags origin HEAD:master
