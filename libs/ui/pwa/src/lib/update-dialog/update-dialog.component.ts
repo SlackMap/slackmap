bd670ee7e08d2b37b09bb5b4b0a772d37f320456
myflowpl <pedro.blaszczak@gmail.com>
2020-03-15 15:35:48 +0100

    refactor: ngsw config

--- libs/ui/pwa/src/lib/update-dialog/update-dialog.component.ts
+++ libs/ui/pwa/src/lib/update-dialog/update-dialog.component.ts
@@ -3,7 +3,6 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface AppData {
   version: string;
-  changelog: string;
 }
 export interface Version {
   hash: string;
